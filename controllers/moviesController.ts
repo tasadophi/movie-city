8815d274103914bc769e0153d1a0037d8a6a7a12
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2024-04-16 21:06:11 +0330

    feat: implement getOne movie controller

--- controllers/moviesController.ts
+++ controllers/moviesController.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from "express";
 import MovieModel from "@/models/movieModel";
 
-const getAllMovies = async (req: Request, res: Response) => {
-  const movies = await MovieModel.find();
-  res.json({ status: "success", data: movies });
+const getAll = async (req: Request, res: Response) => {
+  try {
+    const movies = await MovieModel.find();
+    res.status(200).json({ status: "success", data: movies });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      data: { message: "Some problem on getting movies, try again." },
+    });
+  }
+};
+
+const getOne = async (req: Request, res: Response) => {
+  try {
+    const movie = await MovieModel.findOne({ _id: req.params.id });
+    res.status(200).json({ status: "success", data: movie });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      data: { message: "Some problem on getting movie, try again." },
+    });
+  }
 };
 
 const moviesController = {
-  getAllMovies,
+  getAll,
+  getOne,
 };
 
 export default moviesController;
