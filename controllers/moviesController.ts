740929de97c1eab93d70ae8d97fa05edb3e3214c
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2024-04-16 21:22:20 +0330

    feat: implement getTops movies controller

--- controllers/moviesController.ts
+++ controllers/moviesController.ts
@@ -25,9 +25,22 @@ const getOne = async (req: Request, res: Response) => {
   }
 };
 
+const getTops = async (req: Request, res: Response) => {
+  try {
+    const movies = await MovieModel.find({ top: true });
+    res.status(200).json({ status: "success", data: movies });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      data: { message: "Some problem on getting movies, try again." },
+    });
+  }
+};
+
 const moviesController = {
   getAll,
   getOne,
+  getTops,
 };
 
 export default moviesController;
